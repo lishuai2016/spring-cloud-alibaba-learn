a81d04e0b02e57515a83fa08d5a0ec9117d0a2a8
lishuai2016 <1830473670@qq.com>
2020-11-07 14:58:41 +0800

    社会化服务治理demo

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+spring-cloud-alibaba相关组件学习
+
+https://spring.io/projects/spring-cloud-alibaba 
---
+++ src/main/java/com/ls/Main.java
@@ -0,0 +1,12 @@
+package com.ls;
+
+/**
+ * @program: spring-cloud-alibaba-learn
+ * @author: lishuai
+ * @create: 2020-11-07 11:22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("spring-cloud-alibaba-learn");
+    }
+}
